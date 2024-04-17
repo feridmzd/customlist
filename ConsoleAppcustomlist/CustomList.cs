fe39f1cae20202319a426a8678988005681bc0c7
Ferid <fmemmedzade60@gmail.com>
2024-04-18 02:16:46 +0400

    task added

---
+++ ConsoleAppcustomlist/CustomList.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppcustomlist
+{
+    
+        public class CustomList<T>
+        {
+            private List<T> Listler;
+
+            public CustomList()
+            {
+                Listler = new List<T>();
+            }
+
+            public void Add(T item)
+            {
+                Listler.Add(item);
+            }
+
+            public bool Remove(T item)
+            {
+                return Listler.Remove(item);
+            }
+
+            public int Count()
+            {
+                return Listler.Count;
+            }
+
+            public T GetItem(int index)
+            {
+                if (index >= 0 && index < Listler.Count)
+                {
+                    return Listler[index];
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException("Index sehv daxil edilib");
+                }
+            }
+
+            public void Print()
+            {
+                foreach (var item in Listler)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+
---
+++ ConsoleAppcustomlist/Program.cs
@@ -0,0 +1,34 @@
+﻿using ConsoleAppcustomlist;
+using System;
+using System.Collections.Generic;
+
+
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        CustomList<int> myList = new CustomList<int>();
+
+        myList.Add(1);
+        myList.Add(2);
+        myList.Add(3);
+        myList.Add(4);
+
+ 
+
+        Console.WriteLine("Count: " + myList.Count());
+
+        Console.WriteLine("Items:");
+        myList.Print();
+
+        Console.WriteLine("Remove  3 cunu: " + myList.Remove(3));
+
+        Console.WriteLine("Count: " + myList.Count());
+
+        Console.WriteLine("Items:");
+        myList.Print();
+
+       
+    }
+}
